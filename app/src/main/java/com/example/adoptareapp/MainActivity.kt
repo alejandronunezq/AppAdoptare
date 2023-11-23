54400e5a5308bde28cf2ed3c71dd7bc6262c41a5
Daniel Nuñez <202160003@ucc.mx>
2023-11-22 20:52:45 -0600

    Agregue layouts

--- app/src/main/java/com/example/adoptareapp/MainActivity.kt
+++ app/src/main/java/com/example/adoptareapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.adoptareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.scoreboard.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
